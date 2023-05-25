299e167722a95fa33edf7ab9a0012d44067b0e62
huynq <huynguyenquang@vccorp.vn>
2023-05-26 01:37:00 +0700

    init project

---
+++ src/views/pages/authentication/auth-forms/AuthLogin.jsx
@@ -0,0 +1,160 @@
+import { useState } from 'react';
+
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack
+  // Typography
+} from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
+// third party
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+// project imports
+import useScriptRef from '~/hooks/useScriptRef';
+import AnimateButton from '~/ui-component/extended/AnimateButton';
+
+// assets
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+const FirebaseLogin = ({ ...others }) => {
+  const theme = useTheme();
+  const scriptedRef = useScriptRef();
+  const [checked, setChecked] = useState(true);
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <Formik
+        initialValues={{
+          username: '',
+          password: '',
+          submit: null
+        }}
+        validationSchema={Yup.object().shape({
+          username: Yup.string().matches(/\S/, 'Tên người dùng không hợp lệ').max(255).required('Vui lòng nhập tên người dùng'),
+          password: Yup.string().max(255).required('Vui lòng nhập mật khẩu')
+        })}
+        onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+          try {
+            if (scriptedRef.current) {
+              setStatus({ success: true });
+              setSubmitting(false);
+            }
+          } catch (err) {
+            console.error(err);
+            if (scriptedRef.current) {
+              setStatus({ success: false });
+              setErrors({ submit: err.message });
+              setSubmitting(false);
+            }
+          }
+        }}
+      >
+        {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+          <form noValidate onSubmit={handleSubmit} {...others}>
+            <FormControl fullWidth error={Boolean(touched.username && errors.username)} sx={{ ...theme.typography.customInput }}>
+              <InputLabel htmlFor="outlined-adornment-username-login">Username</InputLabel>
+              <OutlinedInput
+                size="small"
+                id="outlined-adornment-username-login"
+                type="text"
+                value={values.username}
+                name="username"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                label="Username"
+                inputProps={{}}
+              />
+              <FormHelperText
+                error
+                id="standard-weight-helper-text-username-login"
+                style={{ visibility: touched.username && errors.username ? 'visible' : 'hidden', display: 'block' }}
+              >
+                {errors.username}
+              </FormHelperText>
+            </FormControl>
+
+            <FormControl fullWidth error={Boolean(touched.password && errors.password)} sx={{ ...theme.typography.customInput }}>
+              <InputLabel htmlFor="outlined-adornment-password-login">Password</InputLabel>
+              <OutlinedInput
+                size="small"
+                id="outlined-adornment-password-login"
+                type={showPassword ? 'text' : 'password'}
+                value={values.password}
+                name="password"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                      size="large"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+                inputProps={{}}
+              />
+              <FormHelperText
+                error
+                id="standard-weight-helper-text-password-login"
+                style={{ visibility: touched.password && errors.password ? 'visible' : 'hidden', display: 'block' }}
+              >
+                {errors.password}
+              </FormHelperText>
+            </FormControl>
+            <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1}>
+              <FormControlLabel
+                control={
+                  <Checkbox checked={checked} onChange={(event) => setChecked(event.target.checked)} name="checked" color="primary" />
+                }
+                label="Ghi nhớ mật khẩu"
+              />
+              {/* <Typography variant="subtitle1" color="secondary" sx={{ textDecoration: 'none', cursor: 'pointer' }}>
+                Forgot Password?
+              </Typography> */}
+            </Stack>
+            <Box sx={{ mt: 3, visibility: errors.submit ? 'visible' : 'hidden' }}>
+              <FormHelperText error>{errors.submit}</FormHelperText>
+            </Box>
+
+            <Box sx={{ mt: 2 }}>
+              <AnimateButton>
+                <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="secondary">
+                  Đăng nhập
+                </Button>
+              </AnimateButton>
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default FirebaseLogin;
