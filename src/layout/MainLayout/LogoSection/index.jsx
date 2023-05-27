935166175adc81740aac1eac9529e3a7336fc5c2
huynq <huynguyenquang@vccorp.vn>
2023-05-27 12:22:22 +0700

    Add: Add redux-toolkit vào project. Thay thế redux ban đầu

--- src/layout/MainLayout/LogoSection/index.jsx
+++ src/layout/MainLayout/LogoSection/index.jsx
@@ -1,21 +1,18 @@
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
 
 // material-ui
 import { ButtonBase } from '@mui/material';
 
 // project imports
 import config from '~/config';
+import { useCustomizationStore } from '~/hooks/customization';
 import Logo from '~/ui-component/Logo';
-import { MENU_OPEN } from '~/store/actions';
-
-// ==============================|| MAIN LOGO ||============================== //
 
 const LogoSection = () => {
-  const defaultId = useSelector((state) => state.customization.defaultId);
-  const dispatch = useDispatch();
+  const { customizationState, dispatchMenuOpen } = useCustomizationStore();
+  const defaultId = customizationState.defaultId;
   return (
-    <ButtonBase disableRipple onClick={() => dispatch({ type: MENU_OPEN, id: defaultId })} component={Link} to={config.defaultPath}>
+    <ButtonBase disableRipple onClick={() => dispatchMenuOpen(defaultId)} component={Link} to={config.defaultPath}>
       <Logo />
     </ButtonBase>
   );
